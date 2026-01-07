eefdeab40ed1609b107279ed410761d7574e933a
rafaGarciaT <rafael_garcia@educadventista.org>
2026-01-07 17:57:49 -0300

    fix: Aprimora os fetchs

--- src/fetch/fetch_cdi.py
+++ src/fetch/fetch_cdi.py
@@ -2,18 +2,39 @@ from datetime import datetime, timedelta
 import pandas as pd
 import requests
 
+class CdiFetchError(Exception):
+    pass
 
 def get_cdi(date: datetime) -> float:
-    prev_date = date - timedelta(days=1)
     url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados"
     params = {
         "formato": "json",
-        "dataInicial": prev_date.strftime("%d/%m/%Y"),
-        "dataFinal": date.strftime("%d/%m/%Y"),
+        "dataInicial": date.strftime("%d/%m/%Y"),
+        "dataFinal": date.strftime("%d/%m/%Y")
     }
-
     response = requests.get(url, params=params)
-
     data = response.json()
     df = pd.DataFrame(data)
-    return df["valor"].astype(float)[0]
+
+    if "erro" in df.columns:
+        error = df["erro"]
+        raise CdiFetchError(f"Dados de CDI não encontrados para a data {date.strftime('%Y-%m-%d')}: {error}")
+
+    return df["valor"].astype(float).iloc[0]
+
+
+def get_cdi_range(start_date: datetime, end_date: datetime) -> list[dict[str, float]]:
+    cdi_list = []
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados"
+    params = {
+        "formato": "json",
+        "dataInicial": start_date.strftime("%d/%m/%Y"),
+        "dataFinal": end_date.strftime("%d/%m/%Y")
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    df = pd.DataFrame(data)
+
+    for cdi in df.itertuples():
+        cdi_list.append({cdi.data: float(cdi.valor)})
+    return cdi_list
--- src/fetch/fetch_ipca.py
+++ src/fetch/fetch_ipca.py
@@ -4,17 +4,38 @@ import pandas as pd
 import requests
 
 
+class IpcaFetchError(Exception):
+    pass
+
 def get_ipca(date: datetime) -> float:
-    prev_month = date - relativedelta(months=1)
     url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados"
     params = {
         "formato": "json",
-        "dataInicial": f"01/{prev_month.strftime('%m/%Y')}",
-        "dataFinal": f"02/{date.strftime('%m/%Y')}",
+        "dataInicial": f"{date.strftime('%d/%m/%Y')}",
+        "dataFinal": f"{date.strftime('%d/%m/%Y')}",
     }
-
     response = requests.get(url, params=params)
-
     data = response.json()
     df = pd.DataFrame(data)
-    return df["valor"].astype(float)[0]
+
+    if "erro" in df.columns:
+        error = df["erro"]
+        raise IpcaFetchError(f"Dados de IPCA não encontrados para a data {date.strftime('%Y-%m-%d')}: {error}")
+
+    return df["valor"].astype(float).iloc[0]
+
+def get_ipca_range(start_date: datetime, end_date: datetime) -> list[dict[str, float]]:
+    ipca_list = []
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados"
+    params = {
+        "formato": "json",
+        "dataInicial": start_date.strftime("%d/%m/%Y"),
+        "dataFinal": end_date.strftime("%d/%m/%Y")
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    df = pd.DataFrame(data)
+
+    for ipca in df.itertuples():
+        ipca_list.append({ipca.data: float(ipca.valor)})
+    return ipca_list
